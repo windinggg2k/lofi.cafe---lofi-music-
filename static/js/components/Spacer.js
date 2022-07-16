9ec65fe7cf976d4c0c091acee31ca4863ba8ecd7
Your Name <you@example.com>
2022-07-16 15:30:33 +0700

    first commit

---
+++ static/js/components/Spacer.js
@@ -0,0 +1,6 @@
+const Spacer = ({ size = 10 }) => {
+    return <div style={{ width: size, height: size }} />;
+  };
+  
+  export default Spacer;
+  
---
+++ static/js/stations.js
@@ -0,0 +1,71 @@
+const stations = [
+    {
+      id: "5qap5aO4i9A",
+      name: "lofi hip hop radio - beats to relax/study to",
+    },
+    {
+      id: "ORBwkXsUNEs",
+      name: "4K 🔴 Lofi Hip Hop Beats 24/7 Radio 🔴 No Copyright Lofi Beats to sleep/ study to",
+    },
+    {
+      id: "DWcJFNfaw9c",
+      name: "lofi hip hop radio - beats to sleep/chill to",
+    },
+    {
+      id: "5yx6BWlEVcY",
+      name: "Chillhop Radio - jazzy & lofi hip hop beats",
+    },
+  
+    {
+      id: "-9gEgshJUuY",
+      name: "Japanese Lofi Radio 24/7 🔴 Aesthetic Lofi Hip Hop Music 🔴",
+    },
+    {
+      id: "-5KAN9_CzSA",
+      name: "coffee shop radio // 24/7 lofi hip-hop beats",
+    },
+  
+    {
+      id: "Qt0-9mO-ZXY",
+      name: "Space Lofi Hip Hop Radio 24/7 🚀 Chill Lofi Beats To Study, Lofi Sleep Music 🚀 No Copyright Lofi",
+    },
+  
+    {
+      id: "kJ3gKk-Zu-8",
+      name: "Lofi Ramen shop ♫ chillhop beats to chill & study ♫ relaxing lofi music",
+    },
+  
+    {
+      id: "HDhR2Yhnvfo",
+      name: "lofi hip hop radio 🌿 chill beats to relax/study to",
+    },
+  
+    {
+      id: "7NOSDKb0HlU",
+      name: "lofi hip hop radio - beats to study/relax to 🐾",
+    },
+  
+    {
+      id: "tNkZsRW7h2c",
+      name: "Space Ambient Music LIVE 24/7: Space Traveling Background Music, Music for Stress Relief, Dreaming",
+    },
+    {
+      id: "IjMESxJdWkg",
+      name: "24/7 lofi hip hop radio - beats to study/chill/relax",
+    },
+    {
+      id: "CGVYpiS3lTg",
+      name: "Friday vibes ~ lofi hiphop radio 24/7 - beats to relax/study",
+    },
+    {
+      id: "QYW2K6Ncfwo",
+      name: "SACRA BEATS サクラビーツ | lofi beats,24/7,chill,relax,study to,radio,",
+    },
+    {
+      id: "qH3fETPsqXU",
+      name: "【24/7 CHILL LOFI HIP HOP RADIO】beats to sleep/relax/study to",
+    },
+  ];
+  
+  export default stations;
+  
